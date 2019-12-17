a065c1496ffe5868585ec1db7b5966d602789cc3
Justin Swiger <justin.swiger@me.com>
2019-12-17 12:32:26 -0500

    initial commit

---
+++ Controllers/MumblingController.cs
@@ -0,0 +1,10 @@
+namespace ExplosionApi.Controllers
+{
+  [ApiController]
+  [Route("api/[controller]")]
+  public class MumblingController : ControllerBase
+  {
+    [HttpGet]
+    public ActionResult 
+  }
+}
---
+++ Migrations/20191115194718_AddedThings.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace ExplosionApi.Migrations
+{
+    public partial class AddedThings : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Things",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Things", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Things");
+        }
+    }
+}
